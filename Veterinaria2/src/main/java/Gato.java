28d5918bd84956e0e7c85991230e975dbfb24a42
Sebas09210426 <sebasgalvis0426@gmail.com>
2025-10-02 20:53:22 +0000

    Add files via upload

---
+++ Veterinaria2/src/main/java/Gato.java
@@ -0,0 +1,78 @@
+public class Gato extends Mascota{
+    //Atributos
+    private String especie;
+    private String raza;
+    private String estado;
+    private double cantidadDeHorasDeSuenoEnPromedio;
+    private int nivelDeIndependencia;
+
+    //Consturctor
+
+    public Gato(String nombre, int edad, double peso, String identificacion, String nombrePropietario, String numeroPropietario, String especie, String raza, String estado, double cantidadDeHorasDeSuenoEnPromedio, int nivelDeIndependencia) {
+        super(nombre, edad, peso, identificacion, nombrePropietario, numeroPropietario);
+        this.especie = especie;
+        this.raza = raza;
+        this.estado = estado;
+        this.cantidadDeHorasDeSuenoEnPromedio = cantidadDeHorasDeSuenoEnPromedio;
+        this.nivelDeIndependencia = nivelDeIndependencia;
+    }
+
+
+    //Getters y Setters
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public double getCantidadDeHorasDeSuenoEnPromedio() {
+        return cantidadDeHorasDeSuenoEnPromedio;
+    }
+
+    public void setCantidadDeHorasDeSuenoEnPromedio(double cantidadDeHorasDeSuenoEnPromedio) {
+        this.cantidadDeHorasDeSuenoEnPromedio = cantidadDeHorasDeSuenoEnPromedio;
+    }
+
+    public int getNivelDeIndependencia() {
+        return nivelDeIndependencia;
+    }
+
+    public void setNivelDeIndependencia(int nivelDeIndependencia) {
+        this.nivelDeIndependencia = nivelDeIndependencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota{" +
+                "nombre='" + nombre + "\n" +
+                ", edad=" + edad +
+                ", identificacion='" + identificacion + "\n" +
+                ", nombrePropietario='" + nombrePropietario + "\n" +
+                ", numeroPropietario='" + numeroPropietario + "\n" +
+                ", especie='" + especie + "\n" +
+                ", raza='" + raza + "\n" +
+                ", estado='" + estado + "\n" +
+                ", cantidad de horas de sueño en promedio: " + cantidadDeHorasDeSuenoEnPromedio +
+                ", nivel de independencia: " + nivelDeIndependencia +
+                '}';
+    }
+}
---
+++ Veterinaria2/src/main/java/Reptil.java
@@ -0,0 +1,5 @@
+public class Reptil extends Mascota{
+    //Atributos
+    private String especie;
+    private String habitatRequerido
+}
