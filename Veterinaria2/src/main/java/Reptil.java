fd88f5e614edfbd67c13ed88374027c4a8247e82
kevinR2005-dev <kevin.robim@uqvirtual.edu.co>
2025-10-06 11:54:53 -0500

    Update Reptil.java

--- Veterinaria2/src/main/java/Reptil.java
+++ Veterinaria2/src/main/java/Reptil.java
@@ -1,5 +1,24 @@
 public class Reptil extends Mascota{
+
     //Atributos
-    private String especie;
-    private String habitatRequerido
+    private enum habitatRequerido{terrestre, acuatico, mixto};
+    private int temperaturaOptima;
+    private enum nivelPeligrosidad{inofensivo, medio, alto};
+
+    public Reptil(String nombre,String especie, String raza, int edad, double peso, String identificacion, String nombrePropietario, String numeroPropietario, int temperaturaOptima) {
+        super(nombre, especie, raza, edad, peso, identificacion, nombrePropietario, numeroPropietario);
+    }
+
+
+    /* Gets y sets */
+
+    public int  getTemperaturaOptima() {
+        return temperaturaOptima;
+    }
+    public void setTemperaturaOptima(int temperaturaOptima){
+        this.temperaturaOptima = temperaturaOptima;
+    }
+
+
 }
+
